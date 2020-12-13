281835413ba82cdf69753a04aaf274846a1ff81b
SynTex <syntexdzn@t-online.de>
2020-12-13 20:02:23 +0100

    Light Service Fixes

--- accessories/light.js
+++ accessories/light.js
@@ -9,7 +9,7 @@ module.exports = class LightService extends BaseService
 		Characteristic = manager.platform.api.hap.Characteristic;
 		Service = manager.platform.api.hap.Service;
 		
-		super(homebridgeAccessory, deviceConfig, serviceConfig, Service.ContactSensor, manager);
+		super(homebridgeAccessory, deviceConfig, serviceConfig, Service.LightSensor, manager);
 		
 		homebridgeAccessory.getServiceById(Service.LightSensor, serviceConfig.subtype).getCharacteristic(Characteristic.CurrentAmbientLightLevel).on('get', this.getState.bind(this));
 	
