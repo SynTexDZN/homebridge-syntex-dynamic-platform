e79269621ec042c741c1df6aaab7e9b073e0be64
SynTexDZN <syntexdzn@t-online.de>
2021-02-23 00:09:29 +0100

    Naming Fixes

--- src/base.js
+++ src/base.js
@@ -10,7 +10,7 @@ module.exports = class BaseService
 		this.name = serviceConfig['name'];
 
 		homebridgeAccessory.context.id = deviceConfig['id'];
-		homebridgeAccessory.context.name = serviceConfig['name'];
+		homebridgeAccessory.context.name = deviceConfig['name'];
 
 		var subtype = serviceConfig.subtype;
 
